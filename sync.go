3e69fdf5b22e3d4e00bd8b1a82729c72aef80df4
dusterio <denis@mysenko.com>
2016-10-11 17:37:11 +1100

    r Small optimizations

--- sync.go
+++ sync.go
@@ -13,8 +13,7 @@ func getEnv(name string) string {
 	value := os.Getenv(name)
 
 	if len(value) == 0 {
-		log.Println("Please set " + name + " variable")
-		os.Exit(1)
+		panic("Please set " + name + " variable")
 	}
 
 	return value
@@ -27,16 +26,15 @@ func getNodeIps() []string {
 	nodeList, err := dockercloud.ListNodes()
 
 	if err != nil {
-  		log.Println(err)
-	} else {
-		log.Println("Received public IP list from Docker Cloud")
+  		panic(err)
 	}
 
+	log.Println("Received public IP list from Docker Cloud")
+	
 	nodeIps := make([]string, 0)
 
 	if len(nodeList.Objects) == 0 {
-		log.Println("There are no nodes in your Docker Cloud account")
-		os.Exit(1)
+		log.Println("There are no nodes in your Docker Cloud account yet")
 	}
 
 	for i := 0; i < len(nodeList.Objects); i++ {
@@ -73,28 +71,27 @@ func listenToEvents() {
  * Rewrite inbound rules for the security group
  */
 func modifySecurityGroup(groupId string, ips []string) {
-	var inboundRules ec2.AuthorizeSecurityGroupIngressInput
-	var flushRules ec2.RevokeSecurityGroupIngressInput
+	var newRules ec2.AuthorizeSecurityGroupIngressInput
+	var oldRules ec2.RevokeSecurityGroupIngressInput
     var allProtocol string = "-1"
 
     log.Println("Flushing security group... ")
 
 	svc := ec2.New(session.New())
-    inboundRules.GroupId = &groupId
-    flushRules.GroupId = &groupId
+    newRules.GroupId = &groupId
+    oldRules.GroupId = &groupId
 
 	params := &ec2.DescribeSecurityGroupsInput{ GroupIds: []*string{ &groupId }}
 	resp, err := svc.DescribeSecurityGroups(params)
 
 	for i := 0; i < len(resp.SecurityGroups[0].IpPermissions); i++ {
 		existing := resp.SecurityGroups[0].IpPermissions[i]
-		flushRules.IpPermissions = append(flushRules.IpPermissions, existing)
+		old.IpPermissions = append(oldRules.IpPermissions, existing)
     }
 	
-	_, err = svc.RevokeSecurityGroupIngress(&flushRules)
-
+	_, err = svc.RevokeSecurityGroupIngress(&oldRules)
 	if err == nil {
-		log.Println("done")
+		log.Println("success")
 	}
 
 	log.Println("Adding current node IPs to the group... ")
@@ -106,16 +103,15 @@ func modifySecurityGroup(groupId string, ips []string) {
 	    entry := new(ec2.IpPermission)
     	entry.IpProtocol = &allProtocol
 	    entry.IpRanges = []*ec2.IpRange{{CidrIp: &ips[i]}}
-    	inboundRules.IpPermissions = append(inboundRules.IpPermissions, entry)
+    	newRules.IpPermissions = append(newRules.IpPermissions, entry)
 	}
 
-    _, err = svc.AuthorizeSecurityGroupIngress(&inboundRules)
-    
+    _, err = svc.AuthorizeSecurityGroupIngress(&newRules)
     if err != nil {
 		panic(err)
-	} else {
-		log.Println("done")
 	}
+
+	log.Println("success")
 }
 
 /*
@@ -130,6 +126,9 @@ func initDockerCloud() {
 	}
 }
 
+/*
+ * Main block
+ */
 func main() {
 	initDockerCloud()
 
